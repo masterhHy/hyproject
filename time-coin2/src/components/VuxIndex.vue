def5b33249eb994e20825e51be37ec75b9ae4fd0
wuliya <wuliya@iplas.com.cn>
2018-10-09 17:36:03 +0800

    删除旧项目，增加新项目

---
+++ time-coin2/src/components/VuxIndex.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <group title="测试" title-color="red">
+      <cell title="测试一" is-link>
+        11111111
+      </cell>
+      <cell  title="测试一" is-link is-loading>
+        2222
+      </cell>
+    </group>
+  </div>
+</template>
+
+<script>
+  import {Group, Cell} from 'vux'
+
+  export default {
+    name: "VuxIndex",
+    components: {Group, Cell}
+  }
+</script>
+
+<style scoped>
+
+</style>
