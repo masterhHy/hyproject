095bc9adb1c87830664b0f8a65abe0f59906f518
wuliya <wuliya@iplas.com.cn>
2018-10-09 18:57:40 +0800

    提交底部导航栏，同时进行代码修改。

--- time-coin2/src/components/h5/public/TabBar.vue
+++ time-coin2/src/components/h5/public/TabBar.vue
@@ -2,16 +2,21 @@
   <div>
     <tabbar>
       <tabbar-item selected link="/component/demo">
-        <img slot="icon" src="../../../assets/logo.png">
-        <span slot="label">时间金币</span>
+        <i class="fa fa-camera-retro  fa-2x" slot="icon"></i>
+        <!--<span class="fa fa-plus inline-block" slot="icon"></span>-->
+        <!-- <img slot="icon" src="../../../assets/images/icon.png">-->
+        <i slot="icon-active" class="fa fa-camera-retro fa-2x"></i>
+        <span slot="label">1</span>
       </tabbar-item>
-      <tabbar-item show-dot>
-        <img slot="icon" src="../../../assets/logo.png">
-        <span slot="label">愿望清单</span>
+      <tabbar-item>
+        <img slot="icon" src="../../../assets/images/week.png">
+        <img slot="icon-active" src="../../../assets/logo.png">
+        <span slot="label">2</span>
       </tabbar-item>
-      <tabbar-item badge="2">
-        <img slot="icon" src="../../../assets/logo.png">
-        <span slot="label">我的</span>
+      <tabbar-item>
+        <img slot="icon" src="../../../assets/images/icons8-user-24.png">
+        <img slot="icon-active" src="../../../assets/logo.png">
+        <span slot="label">3</span>
       </tabbar-item>
     </tabbar>
   </div>
@@ -28,7 +33,9 @@
     }
   }
 </script>
-
 <style scoped>
 
+  .blue {
+    color: red;
+  }
 </style>
