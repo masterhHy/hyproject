f335899b14ca0865104e7463e5496ffe149fcdbc
guo <532246081@qq.com>
2018-10-10 15:13:28 +0800

    时间金币后台客户端创建

---
+++ time-coin-client/src/main/java/com/hao/time/entity/table/TableTimeCoin.java
@@ -0,0 +1,80 @@
+package com.hao.time.entity.table;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name="time_coin")
+@Table(name="time_coin",indexes={@Index(name="time_markday_ix",columnList="markDay")})
+public class TableTimeCoin {
+    @Id
+    private String id;
+    @Column(columnDefinition="int(3) COMMENT '金币类型'")
+    private Integer type;
+    @Column(columnDefinition="text COMMENT '金币含义'")
+    private String remark;
+    @Column(columnDefinition="varchar(255) COMMENT '金币记录天'")
+    private String markDay;
+    @Column(columnDefinition="varchar(255) COMMENT '用户表id'")
+    private String userId;
+
+    @Column(columnDefinition="datetime COMMENT '创建时间'")
+    private Date createTime;
+    @Column(columnDefinition="datetime COMMENT '创建时间'")
+    private Date updateTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getMarkDay() {
+        return markDay;
+    }
+
+    public void setMarkDay(String markDay) {
+        this.markDay = markDay;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
---
+++ time-coin-client/src/test/java/test/Code.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.cglib.beans.BeanCopier.Generator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Code {
+
+	public static void main(String[] args) throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(Generator.class.getResourceAsStream("/generatorConfig.xml"));
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
+	}
+}
