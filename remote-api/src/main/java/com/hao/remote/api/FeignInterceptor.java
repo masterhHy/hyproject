d4c436a54533ef122c8596ef35b63351e2cb718c
Hy <532246081@qq.com>
2018-11-02 21:03:31 +0800

    提交文件

--- remote-api/src/main/java/com/hao/remote/api/FeignInterceptor.java
+++ remote-api/src/main/java/com/hao/remote/api/FeignInterceptor.java
@@ -4,6 +4,10 @@ import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import org.springframework.context.annotation.Configuration;
 
+/*
+ * 这项目主要存放各个微服务对外开放的接口
+ */
+
 /**
  * feign拦截器 给后台应用请求加上head 方便权限控制放开
  */
