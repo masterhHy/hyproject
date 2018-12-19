17412fc219f8852da3a28139c64dd41e414db4ee
guo <532246081@qq.com>
2018-12-19 12:19:24 +0800

    云配置

--- client-chem/src/main/java/com/hao/chem/ChemClientApplication.java
+++ client-chem/src/main/java/com/hao/chem/ChemClientApplication.java
@@ -1,9 +1,9 @@
 package com.hao.chem;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 
-@SpringBootApplication
+@SpringCloudApplication
 public class ChemClientApplication  {
 
 	public static void main(String[] args){
